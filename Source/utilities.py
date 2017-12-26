e27df1a163ca78db6a96eab1c45c94a8a175d852
Phoenix1747 <apertured@public-files.de>
2017-12-26 14:10:45 +0100

    better comments #4

--- Source/utilities.py
+++ Source/utilities.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
-""" This module contains the functions:
+"""
+This module contains the functions:
 - set_units
 - kabsch
 - form_displacement_matrix
@@ -10,7 +11,7 @@ from sys import exit
 from numpy import pi, column_stack, transpose, linalg, dot, concatenate, outer
 
 def set_units(unit_system):
-    """This function takes care of the units in the boltzmann and planck constant """
+    """This function takes care of the units in the boltzmann and planck constant."""
 
     constants = {"SI": ((6.62606957 * pow(10, -34))/(2*pi), 1.3806488 * pow(10, -23)), \
     "LJ": ((0.18292026)/(2*pi), 1.0), \
@@ -28,7 +29,7 @@ def set_units(unit_system):
         exit('This unit system does not exist !')
 
 def kabsch(atomic_coord, ref):
-    """It applies the kabsch algorithm to determine the rotated coordinates of a molecule """
+    """This function applies the kabsch algorithm to determine the rotated coordinates of a molecule."""
 
     p_matrix = column_stack((atomic_coord[:, 0], atomic_coord[:, 1], atomic_coord[:, 2]))
     q_matrix = column_stack((ref[:, 0], ref[:, 1], ref[:, 2]))
@@ -44,7 +45,7 @@ def kabsch(atomic_coord, ref):
     return p_all
 
 def form_disp_matrix(atomic_coord, square_mass):
-    """"This function computes the displacement matrix of a single configuration"""
+    """This function computes the displacement matrix of a single configuration."""
     atom_coords = concatenate([atomic_coord[:, 0], atomic_coord[:, 1], atomic_coord[:, 2]])
     sq_ext_masses = concatenate([square_mass, square_mass, square_mass])
     disp_matrix = outer(sq_ext_masses, sq_ext_masses)*outer(atom_coords, atom_coords)
